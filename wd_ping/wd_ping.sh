b861727c4f8ec4222b95b17b8b879a180dbfa7f9
anthony <anthony@d77ea471-5fd1-4dec-96fe-2912e94e7bdb>
2012-04-27 11:48:16 +0000

    use non-blocking read from tty
    git-svn-id: svn+ssh://fiord.dyndns.org/svn/repository/dls_administrative/dls_utils/ar_wd@2542 d77ea471-5fd1-4dec-96fe-2912e94e7bdb

--- wd_ping/wd_ping.sh
+++ wd_ping/wd_ping.sh
@@ -1,18 +1,38 @@
 #!/bin/sh
 
 DEV=/dev/ttyACM0
+PING_INTERVAL=60
+READ_GATHER_TIMEOUT=1
 
-/bin/stty -F $DEV clocal raw -echo
+#non-blocking read from $DEV
+/bin/stty -F $DEV clocal raw -echo -icanon time 0 min 0
 
 (
+  exec 3<>$DEV
+  #allow arduino to boot
+  sleep 3
   while true; do
-    echo "ping" > $DEV
+    echo -ne "ping\r" >&3
     res=$?
     if [ $res -ne 0 ]; then
       logger "watchdog pinging failed with exit code=$res"
+      #exit 1
     else
-      logger "watchdog ping result: `cat $DEV`"
+      sleep $READ_GATHER_TIMEOUT
+      #read ret<&3
+
+      ret=''
+      while :
+      do
+        key=$( dd bs=1 count=1 <&3 2> /dev/null; printf "." )
+        key=${key%.}
+        ret="${ret}${key}"
+        if [ "$key" = '' ]; then
+          break
+        fi
+      done
+      logger "watchdog ping result: $ret"
     fi
-    sleep 60
+    sleep `expr $PING_INTERVAL - $READ_GATHER_TIMEOUT`
   done
 ) &
